e93231ab7fc0b95958e9f04289c109716d65a5a9
David Rivadeneyra <david.rivadeneyra.c@gmail.com>
2022-06-29 18:34:51 -0500

    base proyect dos jaja

---
+++ components/layout/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<>
+			<header className='structureHeader'>
+				<img
+					src='img/logo-default.svg'
+					alt='Logo Corporación Romero SCI'
+					className='h-16'
+				/>
+			</header>
+		</>
+	)
+}
+
+export default Header
