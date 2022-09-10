4dcfe7c01e6341d51bf8ace57f4769e699bc2e0d
David Rivadeneyra <david.rivadeneyra.c@gmail.com>
2022-09-09 20:05:17 -0500

    actualizo mapa

--- components/layout/BannerNuestrasInstalaciones.js
+++ components/layout/BannerNuestrasInstalaciones.js
@@ -62,7 +62,7 @@ const BannerNuestrasInstalaciones = () => {
 				<div className='section-container'>
 					<div className='w-full relative h-[400px]'>
 						<iframe
-							src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.9468236180533!2d-77.03573097242884!3d-12.04717982188416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8b67080a16f%3A0xe642875a45db9689!2sPje.%20de%20Jos%C3%A9%20Olaya%2C%20Cercado%20de%20Lima%2015001!5e0!3m2!1ses-419!2spe!4v1660197559311!5m2!1ses-419!2spe'
+							src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3904.7845744448114!2d-77.07362538518935!3d-11.85034759159834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa18cfe1f0e6ac0ad!2zMTHCsDUxJzAxLjMiUyA3N8KwMDQnMTcuMiJX!5e0!3m2!1ses!2spe!4v1662771790072!5m2!1ses!2spe'
 							className='left-0 top-0 h-full md:h-[400] w-full absolute'></iframe>
 					</div>
 				</div>
