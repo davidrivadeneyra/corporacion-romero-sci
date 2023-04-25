9c221417e03a9d5d57aefc6eb1382fb292e205ad
David Rivadeneyra <david.rivadeneyra.c@gmail.com>
2023-04-25 13:25:26 -0500

    nuevos og para facebook y twitter

--- pages/index.js
+++ pages/index.js
@@ -14,18 +14,45 @@ export default function Home() {
 	return (
 		<>
 			<Head>
+				{/* <!-- HTML Meta Tags --> */}
 				<title>
 					Somos Corporación Romero SCI - Especialista en tu seguridad
 				</title>
 				<meta
+					name='description'
+					content='Somos Corporación Romero SCI, especialistas comprometidos con tu seguridad'></meta>
+				<meta
 					name='viewport'
 					content='initial-scale=1.0, width=device-width'
 				/>
+				{/* <!-- Facebook --> */}
+
 				<meta
-					name='Somos Corporación Romero SCI, especialistas comprometidos con tu seguridad'
-					content='https://corporacionromerosci.com/images/cr-og-corporacion-romero.jpg'
+					property='og:url'
+					content='https://corporacionromerosci.com/images/cr-og-corporacion-romero.jpg'></meta>
+				<meta
+					property='og:title'
+					content='Somos Corporación Romero SCI - Especialista en tu seguridad'></meta>
+				<meta property='og:description' content=''></meta>
+				<meta
 					property='og:image'
-				/>
+					content='https://corporacionromerosci.com/images/cr-og-corporacion-romero.jpg'></meta>
+
+				{/* <!-- Twitter --> */}
+				<meta name='twitter:card' content='summary_large_image'></meta>
+				<meta
+					property='twitter:domain'
+					content='corporacionromerosci.com'></meta>
+				<meta property='twitter:url' content=''></meta>
+				<meta
+					name='twitter:title'
+					content='Somos Corporación Romero SCI - Especialista en tu seguridad'></meta>
+				<meta
+					name='twitter:description'
+					content='Somos Corporación Romero SCI - Especialista en tu seguridad'></meta>
+				<meta
+					name='twitter:image'
+					content='https://corporacionromerosci.com/images/cr-og-corporacion-romero.jpg'></meta>
 			</Head>
 			<WhatsAppButton></WhatsAppButton>
 			<HeaderResponsive></HeaderResponsive>
